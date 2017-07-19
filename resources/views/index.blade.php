ec335f230235941a608c40b296a0ffcb3e5c70d0
Pierre Tinard <pierre@tinard.fr>
2017-07-20 01:05:27 +0200

    init and index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts/default')
+
+@section('pagetitle') EdPaper @endsection
+
+@section('navbar')
+<li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
+<li class="nav-item"><a href="/login" class="nav-link">Login</a></li>
+<li class="nav-item"><a href="/register" class="nav-link">Register</a></li>
+@endsection
+
+@section('script')
+@endsection
+
+@section('content')
+<div class="container">
+	<div class="jumbotron">
+		<h1 class="display-3">EdPaper</h1>
+		<p class="lead">EdPaper helps you organizing your paperwork. Seamless and easy.</p>
+		<hr class="my-6">
+		<p class="lead">
+			<a class="btn btn-primary btn-lg" href="/register" role="button">Register</a>
+			<a class="btn btn-info btn-lg" href="/login" role="button">Login</a>
+		</p>
+	</div>
+</div>
+@endsection
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<link rel="stylesheet" media="screen" href="/bootstrap.min.css">
+	{{-- <link rel=stylesheet href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> --}}
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Oswald">
+	<link rel=stylesheet type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+	
+
+	<script src="/jquery.js"></script>
+	<script src="/bootstrap.min.js"></script>
+	<title>@yield('pagetitle')</title>
+	<style>
+	.navbar-brand, .nav-link{
+		font-family:Oswald;
+	}
+	body {
+		margin-top:60px;
+	}
+	.alert {
+		margin-bottom: 10px;
+	}
+	</style>
+	@yield('style')
+	@yield('script')
+	<nav class="navbar navbar-fixed-top navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="/">EdPaper</a>
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div class="collapse navbar-collapse" id="navbar" style="max-height:500px;">
+				<ul class="nav navbar-nav navbar-right">
+					@yield('navbar')
+				</ul>
+			</div>
+		</div>
+	</nav>
+</head>
+
+<body>
+	@yield('content')
+</body>
+<footer>
+<div class="container">
+<div class="row">
+	<h5 class="text-center"><small><i>Made by Pierre T. - <a href="https://github.com/Edraens">Edraens</a>, 2017</i></small></h5>
+</div>
+</div><br />
+</footer>
+
+</html>
