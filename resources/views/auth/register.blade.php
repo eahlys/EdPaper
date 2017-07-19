84cb07516dce6685e136c86fd97f1fba3ebfdc44
Pierre Tinard <pierre@tinard.fr>
2017-07-20 01:16:50 +0200

    language login register pages

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts/default')
 
-@section('pagetitle') Inscription - ChatProject @endsection
+@section('pagetitle') Register - Edpaper @endsection
 
 @section('navbar')
-<li class="nav-item"><a href="/" class="nav-link">Accueil</a></li>
-<li class="nav-item "><a href="/login" class="nav-link">Connexion</a></li>
-<li class="nav-item active"><a href="#" class="nav-link">Inscription</a></li>
+<li class="nav-item"><a href="/" class="nav-link">Home</a></li>
+<li class="nav-item "><a href="/login" class="nav-link">Login</a></li>
+<li class="nav-item active"><a href="#" class="nav-link">Register</a></li>
 @endsection
 
 @section('content')
 <div class="container">
     <div class="row">
-        <h2 class="text-center display-4">Inscription</h2>
+        <h2 class="text-center display-4">Regsiter</h2>
     </div><br />
     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
         {{ csrf_field() }}
 
         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-            <label for="name" class="col-md-4 control-label">Nom</label>
+            <label for="name" class="col-md-4 control-label">Name</label>
 
             <div class="col-md-6">
                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
@@ -31,7 +31,7 @@
         </div>
 
         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-            <label for="email" class="col-md-4 control-label">Adresse e-mail</label>
+            <label for="email" class="col-md-4 control-label">E-mail address</label>
 
             <div class="col-md-6">
                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
@@ -45,7 +45,7 @@
         </div>
 
         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-            <label for="password" class="col-md-4 control-label">Mot de passe</label>
+            <label for="password" class="col-md-4 control-label">Password</label>
 
             <div class="col-md-6">
                 <input id="password" type="password" class="form-control" name="password">
@@ -74,7 +74,7 @@
         <div class="form-group">
             <div class="col-md-6 col-md-offset-4">
                 <button type="submit" class="btn btn-primary">
-                    <i class="fa fa-btn fa-user"></i> Inscription
+                    <i class="fa fa-btn fa-user"></i> Submit
                 </button>
             </div>
         </div>
