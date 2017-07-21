8b58ad143dc2403da8dbf5f14292861a702fda7a
Pierre Tinard <pierre@tinard.fr>
2017-07-21 18:52:04 +0200

    fix doc

--- app/Doc.php
+++ app/Doc.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doc extends Model
 {
+    protected $fillable = [
+        'userId', 'title', 'docname',
+    ];
 	public function categories()
 	{
 		return $this->belongsToMany('App\Categorie');
