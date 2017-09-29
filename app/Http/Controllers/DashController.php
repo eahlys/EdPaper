31b38bbf4fbf2abbcd01ed7425ec6e521bf79edc
Pierre Tinard <pierre@tinard.fr>
2017-09-29 20:11:06 +0200

    Change passwd edit error messages

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -40,13 +40,12 @@ class DashController extends Controller
 		$user->name = Input::get('name');
 		$user->email = Input::get('email');
 		if (Input::get('password') != "") {
-			// dd($request);
-			if (Input::get('password') != Input::get('passwordconfirm')) return Redirect::back()->withErrors(['Password confirmation does not match']);
+			if (Input::get('password') != Input::get('passwordconfirm')) return Redirect::back()->withErrors(['Password confirmation does not match. Please try again']);
 			if (Hash::check(Input::get('currentpassword'), $user->password))
 			{
 				$user->password = Hash::make(Input::get('password'));
 			}
-			else return Redirect::back()->withErrors(['Current password incorrect']);
+			else return Redirect::back()->withErrors(['Current password is incorrect. Please try again.']);
  		}
 		$user->save();
 		return redirect('/account');
